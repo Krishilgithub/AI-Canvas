3413903f796430b6665c3e3e665d65d6b37e71e8
Krishil Agrawal <krishilagrawal026@gmail.com>
2026-03-22 23:38:21 +0530

    fix: use correct vercel system variable for oauth redirect

--- src/lib/supabase/actions.ts
+++ src/lib/supabase/actions.ts
@@ -7,8 +7,7 @@ import { createClient } from './server'
 const getURL = () => {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL
-    process?.env?.VERCEL_URL ?? // Automatically set by Vercel
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel
     'http://localhost:3000/';
   
   // Make sure to include `https://` when not localhost.
